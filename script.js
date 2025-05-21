910321c8f4ed0996f7221a366bb5265517d15d22
EUPHRASY42 <EUPHRASY.NTOLOMEYO@latoile.em-lyon.com>
2025-05-21 17:26:32 +0200

    après midi

--- script.js
+++ script.js
@@ -1,81 +1,95 @@
+const table = document.getElementById("tableau");
+
+
 function patisserie(url) {
-    
+
     fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        
-        const title = document.getElementById("titre");
-        let revue = document.createElement("h1");
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+
+            const title = document.getElementById("titre");
+            let revue = document.createElement("h1");
             title.appendChild(revue);
-            revue.textContent =data["nomCommercial"];
+            revue.textContent = data["nomCommercial"];
             console.log(revue);
-            const paragraphe=document.createElement("p");
-            paragraphe.textContent=data["phraseAccroche"];
-title.appendChild(paragraphe);
+            const paragraphe = document.createElement("p");
+            paragraphe.textContent = data["phraseAccroche"];
+            title.appendChild(paragraphe);
 
-const boutton=document.createElement("button");
-boutton.textContent=data["texteAppelAction"];
-title.appendChild(boutton);
+            const boutton = document.createElement("button");
+            boutton.textContent = data["texteAppelAction"];
+            title.appendChild(boutton);
+            let section = document.createElement("section");
+            section.classList.add("customer")
 
-data.avantagesClients.forEach(element => {
-    const table=document.getElementById("tableau");
-    let avantages=document.createElement("div");
-    avantages.textContent=element;
-    table.appendChild(avantages);
-    
+            data.avantagesClients.forEach(element => {
+                let avantages = document.createElement("div");
 
-});
-            
-        data.produits.forEach(element => {
-        const produits=document.getElementById("produits") ;
-        let creation=document.createElement("div");
-        creation.textContent=element["nom"];
-        let paragraphe=document.createElement("p");
-        paragraphe.textContent=element["description"];
-        // creation.textContent=element["description"];
-        // setAttribute("src","img-url")
-        // creation.setAttribute=element["image-url"];
-        let image=document.createElement("img");
-        image.src=textContent=element["image-url"];
-        produits.appendChild(creation)
-        produits.appendChild(paragraphe);
-        produits.appendChild(image);
-    });
-      data.temoignages.forEach(element => {
-        
-        const commentaires=document.getElementById("commentaires");
-        let creer=document.createElement("h4");
-        creer.textContent=element["prenom"];
-        let expérience=document.createElement("p");
-        expérience.textContent=element["commentaire"];
-        commentaires.appendChild(creer);
-        commentaires.appendChild(expérience);
-      });
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
+                // avantages.classList.add("clients");
+                avantages.textContent = element;
+                section.appendChild(avantages);
+
+
+            });
+            title.appendChild(section);
+
+            data.produits.forEach(element => {
+                const produits = document.getElementById("produits");
+                let creation = document.createElement("div");
+                creation.classList.add("card");
+                let intitule = document.createElement("h3")
+                intitule.textContent = element["nom"];
+                creation.appendChild(intitule)
+                let paragraphe = document.createElement("p");
+                paragraphe.textContent = element["description"];
+                // creation.textContent=element["description"];
+                // setAttribute("src","img-url")
+                // creation.setAttribute=element["image-url"];
+                let image = document.createElement("img");
+                image.src = textContent = element["image-url"];
+                produits.appendChild(creation)
+                creation.appendChild(paragraphe);
+                creation.appendChild(image);
+            });
+            data.temoignages.forEach(element => {
+
+                const commentaires = document.getElementById("commentaires");
+                let division=document.createElement("div");
+                division.classList.add("look");
+                let creer = document.createElement("h4");
+                creer.textContent = element["prenom"];
+                let expérience = document.createElement("p");
+                expérience.textContent = element["commentaire"];
+                commentaires.appendChild(division);
+                division.appendChild(creer);
+                division.appendChild(expérience);
+                document.createElement
+            });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         })
         .catch(error => {
-            
+
             console.error('Erreur lors du fetch :', error);
         });
-        
-    }
-        patisserie('https://js-dynamic-portfolio-data-makerslab-emlyon-cdweb-8f83155c64a0cc.gitlab.io/json/patisserie.json')
+
+}
+patisserie('https://js-dynamic-portfolio-data-makerslab-emlyon-cdweb-8f83155c64a0cc.gitlab.io/json/patisserie.json')
